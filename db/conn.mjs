e4eab88e643fd63fada037ab6dbd20ef41d20a4d
Hema-mv <mvshema@gmail.com>
2025-01-20 20:30:33 -0600

    connectin modified

--- db/conn.mjs
+++ db/conn.mjs
@@ -5,13 +5,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // global configuration
-const mongoURL = "mongodb+srv://sa:abc123.@cluster0.ykl5f.mongodb.net/"//process.env.ATLAS_URI;
+const mongoURL = process.env.ATLAS_URI;
 const db = mongoose.connection;
 
+if (!mongoURL) {
+  console.error('MongoDB connection string is not defined in environment variables');
+  process.exit(1);
+}
+
 // connect to mongo
-mongoose.connect(mongoURL);
-mongoose.connection.once("open", () => {
-  console.log("Connected to mongo");
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+  socketTimeoutMS: 45000 // Increase socket timeout to 45 seconds
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
 });
 
 export default db;
